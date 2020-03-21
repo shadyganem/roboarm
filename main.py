1294c81ac35107f7c4a15080ea32e0e12d90c59e
pi <rotem304@gmail.com>
2020-03-21 12:09:28 +0200

    fixed bugs with the socket thread

--- main.py
+++ main.py
@@ -36,37 +36,37 @@ def update_sensor_vals():
         Humidity , Temperature = DHT11.get_temp_and_hum()
         time.sleep(0.2)
 
-def polling_thread():
-    response_dict = dict()
-    global Temperature
-    global Humidity
-    global program
-    global program_flag
-    while(True):
-        data = urlencode({"humidity":Humidity,"temperature":Temperature}).encode()
-        url = 'http://shadyganem.com/dataExchange.php'
-        try:
-            with urllib.request.urlopen(url,data) as response:
-                res = response.read(100)
-                response_dict = json.loads(res.decode())
+# def polling_thread():
+#     response_dict = dict()
+#     global Temperature
+#     global Humidity
+#     global program
+#     global program_flag
+#     while(True):
+#         data = urlencode({"humidity":Humidity,"temperature":Temperature}).encode()
+#         url = 'http://shadyganem.com/dataExchange.php'
+#         try:
+#             with urllib.request.urlopen(url,data) as response:
+#                 res = response.read(100)
+#                 response_dict = json.loads(res.decode())
 
-            mode = response_dict.get("mode","manual")
-            global program_flag
-            if mode == "manual":
-                command["motor1"] = int(response_dict.get("motor1",90))
-                command["motor2"] = int(response_dict.get("motor2",90))
-                command["motor5"] = int(response_dict.get("motor5",90))
-                command["motor3"] = int(response_dict.get("motor3",90))
-                command["motor4"] = int(response_dict.get("motor4",90))
-            else:
-                print("selected mode = {}".format(mode))
-                program = mode
+#             mode = response_dict.get("mode","manual")
+#             global program_flag
+#             if mode == "manual":
+#                 command["motor1"] = int(response_dict.get("motor1",90))
+#                 command["motor2"] = int(response_dict.get("motor2",90))
+#                 command["motor5"] = int(response_dict.get("motor5",90))
+#                 command["motor3"] = int(response_dict.get("motor3",90))
+#                 command["motor4"] = int(response_dict.get("motor4",90))
+#             else:
+#                 print("selected mode = {}".format(mode))
+#                 program = mode
                 
 
-            for key,val in response_dict.items():
-                print(key+" : "+val)
-        except Exception as e:
-            print("\033[91mConnection with server is down\033[0m {}".format(str(e)))
+#             for key,val in response_dict.items():
+#                 print(key+" : "+val)
+#         except Exception as e:
+#             print("\033[91mConnection with server is down\033[0m {}".format(str(e)))
 
 def main2():
     global Temperature
@@ -80,15 +80,9 @@ def main2():
         client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         client_socket.connect((Host,Port))
         print("connected")
-    except Exception as e:
-        print(f"can't connect to {Host} at port {Port} closing socket")
-        raise e
-    finally:
-        client_socket.close()
 
-    out_msg = f'"humidity":{Humidity},"temperature":{Temperature}"'
-    try:
         while True:
+            out_msg = '{"humidity":"' + str(Humidity) +'","temperature":"' + str(Temperature) + '"}'
             client_socket.send(bytes(out_msg,'utf-8'))
             msg = client_socket.recv(1024)
             print(msg.decode('utf-8'))
